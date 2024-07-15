842efbc9cd1104db09a3f91a0d04deb4c33ee14c
collum <collum.freedman@gmail.com>
2024-07-15 17:51:58 -0400

    add in possible laser interactions and start path finding

--- plugins/lasers/src/lib.rs
+++ plugins/lasers/src/lib.rs
@@ -1,12 +1,73 @@
-use bevy::{prelude::*, utils::petgraph::graph::Edge};
+use bevy::prelude::*;
 use hexx::*;
 
-#[derive(Clone, Copy, Debug)]
-#[derive(Component, Reflect)]
-pub struct Laser;
+pub struct LaserPlugin;
 
-impl Laser {
-    pub const POWER: f32 = 1.;
+impl Plugin for LaserPlugin {
+    fn build(&self, app: &mut App) {}
+}
+
+impl LaserPlugin {
+    fn track_lasers(
+        lasers: Query<(&Position, &Direction), With<Laser>>,
+        colliders: Query<(
+            &Position,
+            Option<&Refraction>,
+            Option<&Amplification>,
+            Option<&Consumption>,
+        )>,
+    ) {
+        for (laser_position, laser_direction) in &lasers {
+            let mut hits = Vec::new();
+            let mut current_source = *laser_position;
+            'outer: loop {
+                hits.push(current_source);
+                let mut steps = 0;
+                let next_neighbor: Position =
+                    current_source.neighbor(laser_direction.to_hex()).into();
+                while steps < 100 {
+                    for (position, option_refraction, option_amplification, option_consumption) in
+                        &colliders
+                    {
+                        if *position == next_neighbor {
+                            current_source = *position;
+                            break 'outer;
+                        }
+                    }
+                    steps += 1
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(SystemSet)]
+pub struct LaserSystems;
+
+#[derive(Clone, Copy, Debug)]
+#[derive(PartialEq, Eq)]
+#[derive(Component, Deref, DerefMut)]
+pub struct Position(Hex);
+
+impl From<Hex> for Position {
+    fn from(value: Hex) -> Self {
+        Self(value)
+    }
+}
+
+impl Position {
+    pub fn ray(self, direction: &Direction, limit: u32) -> Vec<Position> {
+        let mut ray = Vec::new();
+        let mut steps = 1;
+        let mut next_neighbor = self;
+        while steps < limit {
+            next_neighbor = next_neighbor.neighbor(direction.to_hex()).into();
+            ray.push(next_neighbor);
+            steps += 1;
+        }
+        ray
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -21,7 +82,7 @@ pub enum Direction {
 }
 
 impl Direction {
-    fn to_hexx(&self) -> EdgeDirection {
+    fn to_hex(&self) -> EdgeDirection {
         match self {
             Self::North => EdgeDirection::FLAT_NORTH,
             Self::South => EdgeDirection::FLAT_SOUTH,
@@ -33,29 +94,24 @@ impl Direction {
     }
 }
 
-#[derive(Debug)]
-#[derive(Bundle)]
-pub struct LaserBundle {
-    laser: Laser,
-    direction: Direction,
+#[derive(Clone, Copy, Debug)]
+#[derive(Component, Reflect)]
+pub struct Laser;
+
+impl Laser {
+    pub const POWER: f32 = 1.;
 }
 
-#[derive(PartialEq, Eq)]
-#[derive(Component, Deref, DerefMut)]
-pub struct Position(Hex);
+#[derive(Clone, Copy, Debug, Default)]
+#[derive(Component, Reflect)]
 
-fn first_hit(
-    laser_query: Query<(&Position, &Direction), With<Laser>>,
-    tile_query: Query<&Position>,
-) {
-    for (laser_position, laser_direction) in &laser_query {
-        let mut steps = 0;
-        let mut position = laser_position;
-        while steps < 100 {
-            position = position.neighbor(laser_direction.to_hexx());
-            for occupied_position in &tile_query {
-                if position == occupied_position {}
-            }
-        }
-    }
-}
+pub struct Refraction;
+
+#[derive(Clone, Copy, Debug, Default)]
+#[derive(Component, Reflect, Deref, DerefMut)]
+pub struct Amplification(usize);
+
+#[derive(Clone, Copy, Debug, Default)]
+#[derive(Component, Reflect, Deref, DerefMut)]
+
+pub struct Consumption(Entity);
--- plugins/tiles/src/lib.rs
+++ plugins/tiles/src/lib.rs
@@ -1,9 +1,30 @@
-use bevy::prelude::*;
+use bevy::{ecs::system::Command, prelude::*};
 use hexx::*;
 
 #[derive(Clone, Copy, Debug)]
+#[derive(PartialEq, Eq)]
 #[derive(Component, Deref, DerefMut)]
 pub struct Position(Hex);
+
+impl From<Hex> for Position {
+    fn from(value: Hex) -> Self {
+        Self(value)
+    }
+}
+
+impl Position {
+    pub fn ray(self, direction: Direction, limit: u32) -> Vec<Position> {
+        let mut ray = Vec::new();
+        let mut steps = 1;
+        let mut next_neighbor = self;
+        while steps < limit {
+            next_neighbor = next_neighbor.neighbor(direction.to_hex()).into();
+            ray.push(next_neighbor);
+            steps += 1;
+        }
+        ray
+    }
+}
 
 #[derive(Clone, Copy, Debug)]
 #[derive(Component, Reflect)]
@@ -29,21 +50,92 @@ impl Direction {
     }
 }
 
-impl Position {
-    pub fn ray(&self, direction: Direction, limit: u32) -> Vec {
-        let ray = Vec::new();
-        let mut steps = 1;
-        let mut next_neighbor = self;
-        while steps < limit {
-            next_neighbor = next_neighbor.neighbor(direction.to_hex());
-            ray.append(next_neighbor)
+pub fn neighbor_along_ray(
+    source_query: Query<(&Position, &Direction)>,
+    hit_query: Query<&Position>,
+) {
+    for (source_position, source_direction) in &source_query {
+        let ray = source_position.ray(*source_direction, 100);
+
+        for hit_position in &hit_query {
+            if ray.contains(hit_position) {}
         }
-        ray
     }
 }
 
-pub neighbor_along_ray()
-
 pub trait Tile {
-    const POSITION: Position;
+    fn activate(&self) -> impl Command;
+
+    fn on_hit(&self) -> Option<impl Command> {None}
+}
+
+
+
+pub trait MerchAppExt {
+    fn define_merchandise<T>(&mut self)
+    where
+        T: Component + GetTypeRegistration + Merchandise,
+    {
+    }
+}
+
+impl MerchAppExt for App {
+    fn define_merchandise<T>(&mut self)
+    where
+        T: Component + GetTypeRegistration + Merchandise,
+    {
+        let mut registry = self.world_mut().resource_mut::<MerchRegistry>();
+        // registry.register::<T>().unwrap();
+        self.add_systems(Update, MySystemSet::handle_hit_sinks::<T>)
+    }
+}
+
+impl  {
+    fn handle_hit_sinks<T: Tile>(mut commands: Commands, query: Query<(&Sink, &Position, &dyn Tile)>) {
+        // on hit
+        commands.add(t.activate());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MyTile {
+        value: usize,
+    }
+
+    impl Tile for MyTile {
+        fn activate(&self) -> impl Command {
+            TileCommand { value: self.value }
+        }
+    }
+
+    struct TileCommand {
+        value: usize,
+    }
+
+    impl Command for TileCommand {
+        fn apply(self, world: &mut World) {
+            world.insert_resource(CommandComplete);
+        }
+    }
+
+    #[derive(Resource)]
+    pub struct CommandComplete;
+
+    #[test]
+    fn test_command() {
+        let mut app = App::new();
+
+        app.add_plugins(MinimalPlugins);
+
+        fn system(mut commands: Commands) {
+            commands.add(MyTile { value: 10 }.activate());
+        }
+
+        app.add_systems(Startup, system);
+        app.update();
+        app.world.resource::<CommandComplete>();
+    }
 }
