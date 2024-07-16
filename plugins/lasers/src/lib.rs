adff58ec49eb2926d84616d4fec522bfb9221e6c
collum <collum.freedman@gmail.com>
2024-07-15 22:14:49 -0400

    finish laser path tracking

--- plugins/lasers/src/lib.rs
+++ plugins/lasers/src/lib.rs
@@ -4,41 +4,92 @@ use hexx::*;
 pub struct LaserPlugin;
 
 impl Plugin for LaserPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_event::<RefractionEvent>()
+            .add_event::<AmplificationEvent>()
+            .add_event::<ConsumptionEvent>()
+            .add_systems(Update, Self::track_lasers);
+    }
 }
 
 impl LaserPlugin {
     fn track_lasers(
         lasers: Query<(&Position, &Direction), With<Laser>>,
         colliders: Query<(
+            Entity,
             &Position,
             Option<&Refraction>,
             Option<&Amplification>,
             Option<&Consumption>,
         )>,
+        mut refraction_events: EventWriter<RefractionEvent>,
+        mut amplification_events: EventWriter<AmplificationEvent>,
+        mut consumption_events: EventWriter<ConsumptionEvent>,
     ) {
         for (laser_position, laser_direction) in &lasers {
             let mut hits = Vec::new();
             let mut current_source = *laser_position;
-            'outer: loop {
+            let mut current_direction = *laser_direction;
+            'outer: while !hits.contains(&current_source) {
                 hits.push(current_source);
                 let mut steps = 0;
                 let next_neighbor: Position =
-                    current_source.neighbor(laser_direction.to_hex()).into();
+                    current_source.neighbor(current_direction.to_hex()).into();
                 while steps < 100 {
-                    for (position, option_refraction, option_amplification, option_consumption) in
-                        &colliders
+                    for (
+                        collider_entity,
+                        position,
+                        option_refraction,
+                        option_amplification,
+                        option_consumption,
+                    ) in &colliders
                     {
                         if *position == next_neighbor {
-                            current_source = *position;
-                            break 'outer;
+                            if let Some(consumption) = option_consumption {
+                                consumption_events.send(ConsumptionEvent {
+                                    consumer: collider_entity,
+                                });
+                                return;
+                            }
+                            if let Some(refraction) = option_refraction {
+                                refraction_events.send(RefractionEvent {
+                                    refractor: collider_entity,
+                                });
+                                current_source = *position;
+                                current_direction = refraction.new_direction;
+                                break 'outer;
+                            }
+                            if let Some(amplification) = option_amplification {
+                                amplification_events.send(AmplificationEvent {
+                                    amplifier: collider_entity,
+                                });
+                                current_source = *position;
+                                break 'outer;
+                            }
                         }
                     }
                     steps += 1
                 }
+
+                return;
             }
         }
     }
+}
+
+#[derive(Event)]
+pub struct RefractionEvent {
+    pub refractor: Entity,
+}
+
+#[derive(Event)]
+pub struct AmplificationEvent {
+    pub amplifier: Entity,
+}
+
+#[derive(Event)]
+pub struct ConsumptionEvent {
+    pub consumer: Entity,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -70,9 +121,10 @@ impl Position {
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Default)]
 #[derive(Component, Reflect)]
 pub enum Direction {
+    #[default]
     North,
     South,
     Northeast,
@@ -104,14 +156,15 @@ impl Laser {
 
 #[derive(Clone, Copy, Debug, Default)]
 #[derive(Component, Reflect)]
-
-pub struct Refraction;
+pub struct Refraction {
+    new_direction: Direction,
+}
 
 #[derive(Clone, Copy, Debug, Default)]
 #[derive(Component, Reflect, Deref, DerefMut)]
 pub struct Amplification(usize);
 
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Copy, Debug)]
 #[derive(Component, Reflect, Deref, DerefMut)]
 
 pub struct Consumption(Entity);
